bf8dae76983457ac47032f6ae233f9003de77edb
Paul Lysak <paul.lysak@gmail.com>
2017-07-31 19:53:11 +0300

    ScalaJS hello world

---
+++ client_app_scala/src/main/scala/com/aimplicits/gallery/MyApp.scala
@@ -0,0 +1,13 @@
+package com.aimplicits.gallery
+
+import scala.scalajs.js.annotation.JSExport
+
+@JSExport
+object MyApp {
+
+  @JSExport
+  def main(): Unit = {
+    println("hi, ScalaJS!")
+
+  }
+}
