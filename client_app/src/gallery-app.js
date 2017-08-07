b5441e7ec9a102bb3cbe65bd65051991f58f14ac
Paul Lysak <paul.lysak@gmail.com>
2017-08-07 10:11:54 +0300

    login form

--- client_app/src/gallery-app.js
+++ client_app/src/gallery-app.js
@@ -18,6 +18,46 @@ console.log("hi, there!", appConfig)
 //   IdentityPoolId: appConfig.IdentityPoolId
 // });
 
+Vue.component("sign-in-dialog", {
+    props: [],
+    template: `
+    <div>TODO: sign in</div>
+    `
+
+})
+
+Vue.component("sign-in-controls", {
+    props: [],
+    methods: {
+        signIn: function(event) {
+            console.log("ctrls sign in", this.$data.login, this.$data.password)
+            event.preventDefault();//avoid form submission warnings
+            //TODO actual logic of password validation
+            if(this.$data.password == "123") {
+                this.$emit("signedIn", {id: "id:"+this.$data.login, nick: "nick:"+this.$data.login})
+            } else {
+                console.error("Incorrect login or password")
+            }
+        }
+    },
+    data: function() {
+        return {
+            login: "",
+            password: ""
+        }
+    },
+    template: `
+      <form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Login" v-model:value="login"/>
+          <input type="text" class="form-control" placeholder="Password" v-model:value="password"/>
+        </div>
+        <button type="submit" class="btn btn-default" v-on:click="signIn">Sign In</button>
+      </form>
+    `
+})
+
+
 var app = new Vue({
   data: {
     message: 'Hello Vue!',
@@ -25,12 +65,20 @@ var app = new Vue({
         id: null,
         nick: null
       }
+      // ,
+      // dialogs: {signIn: false}
   },
     methods: {
-      signIn: function(event) {
-          console.log("sign in")
-          this.$data.user = {id: "12345", nick: "azazaz"}
-      },
+      // signIn: function(event) {
+      //     console.log("sign in")
+      //     this.$data.dialogs.signIn = true
+      //     this.$data.user = {id: "12345", nick: "azazaz"}
+      // },
+
+    signedIn: function(event) {
+        console.log("Signed in", event)
+            this.$data.user = event;
+        },
       signOut: function(event) {
           console.log("sign out")
           this.$data.user = {}
@@ -44,10 +92,10 @@ var app = new Vue({
           <div class="navbar-header">
             <a class="navbar-brand" href="#">Gallery</a>
           </div>
+          <sign-in-controls v-if="!user.id" v-on:signedIn="signedIn"></sign-in-controls>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
               <p v-if="user.id && user.nick" class="navbar-text">{{user.nick}}</p>
-              <li><a v-if="!user.id" v-on:click="signIn" href="#">Sign in</a></li>
               <li><a v-if="user.id" v-on:click="signOut" href="#">Sign out</a></li>
             </ul>
           </div>
@@ -58,13 +106,15 @@ var app = new Vue({
         <h1>No public content</h1>
         <p>Sign in to view the gallery content</p>
       </div>
+      
+      <!--<sign-in-dialog v-if="dialogs.signIn"></sign-in-dialog>-->
     
 
     </div>
     `
 }).$mount("#app")
 
-app.$data.user = {id: "2314", nick: "alala"}
+// app.$data.user = {id: "2314", nick: "alala"}
 
 var userPool = new CognitoUserPool({
   UserPoolId: appConfig.UserPoolId,
