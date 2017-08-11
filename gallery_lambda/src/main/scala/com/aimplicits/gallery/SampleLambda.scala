bb09ca9c4a1088c3404c85aef439e05fdd6dc670
Paul Lysak <paul.lysak@gmail.com>
2017-08-11 16:47:20 +0300

    First successful response from Lambda

--- gallery_lambda/src/main/scala/com/aimplicits/gallery/SampleLambda.scala
+++ gallery_lambda/src/main/scala/com/aimplicits/gallery/SampleLambda.scala
@@ -1,18 +1,25 @@
 package com.aimplicits.gallery
 
+import com.amazonaws.serverless.proxy.internal.model.{AwsProxyRequest, AwsProxyResponse}
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.Context
 import org.slf4j.LoggerFactory;
+import scala.collection.JavaConverters._
 
-class SampleLambda extends RequestHandler[java.util.Map[String, AnyRef], String]{
+class SampleLambda extends RequestHandler[AwsProxyRequest, AwsProxyResponse]{
   //TODO
-  override def handleRequest(input: java.util.Map[String, AnyRef], context: Context): String = {
+  override def handleRequest(input: AwsProxyRequest, context: Context): AwsProxyResponse = {
     println("stdout: got the request: "+input.toString)
     log.debug("upd: got the request: "+input.toString)
 //    log.info("dbg: got the request: "+input.toString)
 
-    s"""{"msg": "In reponse to: ${input.toString.length}"}""".stripMargin
+//    s"""{"isBase64Encoded": false, "statusCode": 200, "headers": {"Content-Type": "text/plain"}, "body": "Sample response body"}""".stripMargin
+//    s"""{"statusCode": "200", "headers": {"Content-Type": "text/plain"}, "body": "{}"}""".stripMargin
+    s"""{"statusCode": "200", "headers": {"Content-Type": "text/plain"}, "body": {}}""".stripMargin
+//    s"""{"statusCode": 200, "body": "Sample response body"}""".stripMargin
+    new AwsProxyResponse(200, Map("Content-Type" -> "text/plain").asJava, "Sample body")
   }
 
   private val log = LoggerFactory.getLogger(classOf[SampleLambda])
 }
+
